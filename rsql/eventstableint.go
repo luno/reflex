7b2be1af53f60f0163462083c88cf213e98e6cc3
Corver Roos <corver@luno.com>
2019-11-04 13:05:03 +0200

    reflex/rsql: Add InsertWithMetadata to EventsTableInt
    Reviewers: #backend, jonathan
    
    Reviewed By: #backend, jonathan
    
    Differential Revision: https://phabricator.corp.luno.com/D39996

--- rsql/eventstableint.go
+++ rsql/eventstableint.go
@@ -10,9 +10,19 @@ import (
 
 // EventsTableInt wraps reflex EventsTable and provides typed int64 foreign id inserts.
 type EventsTableInt interface {
+	// Insert works as EventsTable.Insert except that foreign id is an int64.
 	Insert(ctx context.Context, tx *sql.Tx, foreignID int64,
 		typ reflex.EventType) (NotifyFunc, error)
+
+	// InsertWithMetadata works as EventsTable.InsertWithMetadata except
+	// that foreign id is an int64.
+	InsertWithMetadata(ctx context.Context, tx *sql.Tx, foreignID int64,
+		typ reflex.EventType, metadata []byte) (NotifyFunc, error)
+
+	// Insert works as EventsTable.ToStream.
 	ToStream(dbc *sql.DB, opts ...reflex.StreamOption) reflex.StreamFunc
+
+	// Clone works as EventsTable.Clone.
 	Clone(opts ...EventsOption) EventsTableInt
 }
 
@@ -28,6 +38,13 @@ func (e *etableint) Insert(ctx context.Context, tx *sql.Tx, foreignID int64,
 	typ reflex.EventType) (NotifyFunc, error) {
 
 	return e.EventsTable.Insert(ctx, tx, strconv.FormatInt(foreignID, 10), typ)
+}
+
+func (e *etableint) InsertWithMetadata(ctx context.Context, tx *sql.Tx, foreignID int64,
+	typ reflex.EventType, metadata []byte) (NotifyFunc, error) {
+
+	return e.EventsTable.InsertWithMetadata(ctx, tx,
+		strconv.FormatInt(foreignID, 10), typ, metadata)
 }
 
 func (e *etableint) Clone(opts ...EventsOption) EventsTableInt {
