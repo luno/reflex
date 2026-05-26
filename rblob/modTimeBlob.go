0617edc69c7056630df5ccc45d5b1a62dde4d6f2
Alice Yang <ayang@luno.com>
2026-05-26 14:49:08 +0200

    Added description for new modTimeBlob

--- rblob/modTimeBlob.go
+++ rblob/modTimeBlob.go
@@ -9,6 +9,15 @@ import (
 	"github.com/luno/jettison/errors"
 )
 
+// --------------------------------------------------------------------------------------------
+// This differs from the regular blob functionality, in that it is designed to resume
+// its cursor based on the comparisions on last modified rather than on lexicographic
+// ordering. Since this is not supported in the AWS ListObjectsV2 client (https://docs.aws.amazon.com/cli/latest/reference/s3api/list-objects-v2.html)
+// (on the day this is documented 2026-05-26). This should only be used for small to
+// medium size S3 buckets. Reason being that the cursor will need to iterate through all
+// the last modified list objects to determine its resume position.
+// --------------------------------------------------------------------------------------------
+
 // modtimeCursor identifies the last-processed S3 object by its key and
 // last-modified time. ModTime is used for ordering; Key breaks ties and
 // provides identity for the resume skip.
